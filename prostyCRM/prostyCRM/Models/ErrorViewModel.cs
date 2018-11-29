5e6bbaacf0cc376052968cdd438ce940bc6ef5d2
Bamkoff <bartek.fabisiak1@gmail.com>
2018-11-29 12:08:05 +0100

    Stworzenie bazy projektu

---
+++ prostyCRM/prostyCRM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace prostyCRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
