35ddc5824a9bb8f071bb03f1625bbc4928c2a13e
Bankoff <bartek.fabisiak1@gmail.com>
2018-12-07 19:54:21 +0100

    Uaktualnienie apki, dodanie modelu, wygenerowanie na jago postwawie kodu kontrolera

---
+++ prostyCRM/MojaApka/MojaApka/Data/MojaApkaContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MojaApka.Models
+{
+    public class MojaApkaContext : DbContext
+    {
+        public MojaApkaContext (DbContextOptions<MojaApkaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MojaApka.Models.User> User { get; set; }
+    }
+}
---
+++ prostyCRM/MojaApka/MojaApka/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MojaApka.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surrname { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Login { get; set; }
+    }
+}
